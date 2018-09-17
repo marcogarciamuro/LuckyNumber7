d480af0124c5bd328746763f228c2cb80a4e8104
marcogarciamuro <magarciamuro@mail.csuchico.edu>
2018-09-17 16:41:44 -0700

    Determines closeness to 7

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-//Author:
+//Author:Marco Garcia
 #include <iostream>
 
 using namespace std;
@@ -12,13 +12,21 @@ int main()
 
 
   //when they guess correctly (7), display:
+  if(guess == 7)
+  {
   cout<<"YES! Lucky number 7!\n";
-
+  }
+  
   //when they guess just off by one (either 6 or 8), display:
-  cout<<"Oh, so close!\n";
-
+  else if (guess == 6||guess == 8)
+  {
+   cout<<"Oh, so close!\n";
+  }
+  
   //when they guess any other number, display:
+  else 
+  {
   cout<<"Waaay off, sorry.\n";
-
+  }
   return 0;
 }
